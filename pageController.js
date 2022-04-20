ab3c203f1fa972553f7c5fac829a4ffbeddf90f1
Jimmie <jimmieriss@gmail.com>
2022-04-20 11:24:52 +0200

    Scraper work for single url. Writing to json.

--- pageController.js
+++ pageController.js
@@ -1,9 +1,22 @@
 const pageScraper = require('./pageScraper');
+const fs = require('fs');
 async function scrapeAll(browserInstance){
     let browser;
     try{
-        brwoser = await browserInstance;
-        await pageScraper.scraper(browser);
+        browser = await browserInstance;
+       
+        let data = await pageScraper.scraper(browser);
+        const ingredients = data[0].split('\n');
+        const description = data [1].split('\n');
+        const url = data[2]; 
+        console.log(ingredients, description, url)
+        await browser.close();
+        fs.writeFile('data.json', JSON.stringify(ingredients, description), 'utf8', (err) => {
+            if(err){
+                return console.log(err)
+            }
+            console.log('Data scraped and written.');
+        });
     } catch(err){
         console.log(`Browser error: ${err}`);
     }
--- pageScraper.js
+++ pageScraper.js
@@ -4,7 +4,16 @@ const scraperObject = {
         let page = await browser.newPage();
         console.log(`Opening ${this.url}`);
         await page.goto(this.url);
-    }
+        // Wait for dom to load
+        await page.waitForSelector('.ingredients-group')
+        let [ingredients, description, url] = await page.evaluate(() =>{
+            const items = document.querySelectorAll('.ingredients-group');
+            ingredients = items[0].innerText;
+            description = items[1].innerText;
+            url = window.location.href;
+            return [ingredients, description, url];
+        })
+        return [ingredients, description, url];
+    }    
 }
-
 module.exports = scraperObject;
